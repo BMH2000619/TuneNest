671544bcad7cc5f16d65e9f6e38a04f3b429b59b
BMH2000619 <baselmohammedb.m@gmail.com>
2025-06-29 11:27:08 +0300

    added to showSongs

---
+++ src/services/ShowSongs.js
@@ -0,0 +1,50 @@
+import Client from './api'
+
+// Get all songs
+export const getAllSongs = async () => {
+  try {
+    const res = await Client.get('/songs')
+    return res.data
+  } catch (error) {
+    throw error
+  }
+}
+
+// Get a single song by ID
+export const getSongById = async (id) => {
+  try {
+    const res = await Client.get(`/songs/${id}`)
+  } catch (error) {
+    throw error
+  }
+}
+
+// Create a new song
+export const createSong = async (songData) => {
+  try {
+    const res = await Client.post('/songs', songData)
+    return res.data
+  } catch (error) {
+    throw error
+  }
+}
+
+// Update a Song
+export const updateSong = async (id, songData) => {
+  try {
+    const res = await Client.put(`/'songs/${id}`, songData)
+    return res.data
+  } catch (error) {
+    throw error
+  }
+}
+
+// Delete a song
+export const deleteSong = async (id) => {
+  try {
+    const res = await Client.delete(`/'songs/${id}`)
+    return res.data
+  } catch (error) {
+    throw error
+  }
+}
